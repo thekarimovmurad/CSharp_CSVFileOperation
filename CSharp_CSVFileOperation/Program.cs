fac69e105b13c5fc9824572136f49af5276377ea
theka <theka@THEKARIMOVMURAD>
2023-01-15 20:10:14 +0100

    Add project files.

---
+++ CSharp_CSVFileOperation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_CSVFileOperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:/Users/theka/OneDrive/Desktop/addresses.csv";
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] fields = line.Split();
+                foreach (string field in fields)
+                {
+                    Console.Write(field + "\t");
+                }
+            }
+        }
+    }
+}
